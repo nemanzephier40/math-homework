0e2d285fb613fd3d0ec3b9a52b6739b3cf4715e2
nemanzephier40 <nemanzephier40@gmail.com>
2025-03-06 15:44:20 +0000

    Add 1.php

---
+++ src/1.php
@@ -0,0 +1,17 @@
+  <?php
+    $week = date("W");
+    switch ($week) {
+      case '1':
+        echo "Weekly math homework assignment 1";
+        break;
+      case '2':
+        echo "Weekly math homework assignment 2";
+        break;
+      case '3':
+        echo "Weekly math homework assignment 3";
+        break;
+      case '4':
+        echo "Weekly math homework assignment 4";
+        break;
+    }
+  ?>
