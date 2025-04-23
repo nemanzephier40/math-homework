d059abcc543aac8e632f28eef6b84590762a2c12
nemanzephier40 <nemanzephier40@gmail.com>
2025-04-23 11:19:02 +0000

    Add 26.php

---
+++ src/26.php
@@ -0,0 +1,11 @@
+<?php
+// This is a simple PHP script to demonstrate basic operations.
+// Example: adding two numbers
+
+$num1 = 5;
+$num2 = 3;
+
+// Addition
+$result = $num1 + $num2;
+echo "The sum of $num1 and $num2 is: $result";
+?>
