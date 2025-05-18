ff7f2300b0d3d10ffb13ae24800e77099730b180
nemanzephier40 <nemanzephier40@gmail.com>
2025-05-18 19:46:01 +0000

    Add 32.php

---
+++ src/32.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * This file is part of MathHomework.
+ *
+ * Copyright (c) 2018 <NAME>
+ *
+ * MathHomework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MathHomework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MathHomework.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// Define your own variables here
+
+?>
