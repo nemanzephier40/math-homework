12824ba5051a618dbda43d5cb11e11d131b52569
nemanzephier40 <nemanzephier40@gmail.com>
2025-05-23 04:18:32 +0000

    Add 34.php

---
+++ src/34.php
@@ -0,0 +1,16 @@
+<?php
+// Sample PHP code to demonstrate basic arithmetic operations
+
+// Adding two numbers
+$number1 = 5;
+$number2 = 3;
+$result = $number1 + $number2;
+
+echo "The sum of $number1 and $number2 is: $result";
+
+// Subtracting one number from another
+$subtraction = 8 - 4;
+$result = $subtraction;
+
+echo "Subtracted 4 from 8 equals: $result";
+?>
